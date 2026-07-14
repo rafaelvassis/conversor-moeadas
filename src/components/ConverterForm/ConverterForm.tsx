39785e8d941715d74715a185b3d8581c508adba5
Rafael Vasconcelos <955502@sga.pucminas.br>
2026-07-14 13:22:44 -0300

    adiciona formulário de conversão com estilos correspondentes

--- src/components/ConverterForm/ConverterForm.tsx
+++ src/components/ConverterForm/ConverterForm.tsx
@@ -1,5 +1,21 @@
 import "./ConverterForm.css";
 
 export default function ConverterForm() {
-  return <form>Formulário</form>;
+  return (
+    <form>
+      <div className="form__row">
+        <label htmlFor="Source-Amount">Valor</label>
+        <input type="text" id="Source-Amount" name="Source-Amount" />
+      </div>
+      <div className="form__row">
+        <label htmlFor="Base-Currency">De: </label>
+        <input type="text" id="Base-Currency" name="Base-Currency" />
+      </div>
+      <div className="form__row">
+        <label htmlFor="Quote-Currency">Para: </label>
+        <input type="text" id="Quote-Currency" name="Quote-Currency" />
+      </div>
+      <button>Converter</button>
+    </form>
+  );
 }
